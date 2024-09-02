bdce90fee8b02cae95c64de80b610ad8b3387bc9
v.samopyt <v.samopyt14@gmail.com>
2024-09-02 20:00:57 +0300

    commit-03
    adding must have middleware

--- src/server.js
+++ src/server.js
@@ -1,9 +1,40 @@
 import express from "express";
+import cors from "cors";
+import pino from "pino-http";
 
 const PORT =6000;
 
 export const setupServer =()=>{
     const app = express();
+
+    const logger = {
+        transport: {
+            target: 'pino-pretty',
+          },
+    };
+
+    app.use(cors());
+    app.use(pino(logger));
+    app.use(express.json());
+
+
+    app.get("/",(reg,res)=>{
+        res.json({message:"hello world"});
+    });
+
+app.use((reg, res)=>{
+    res.status(404).json(
+        {
+        mistake: `the way ${reg.url} not found`
+    });
+});
+
+app.use((err, reg,res)=>{
+    res.status(500).json({
+        message: 'Something went wrong',
+        error: err.message,
+    });
+});
     app.listen(PORT, ()=>console.log(`sever is running on the port${PORT}`));
 
 };
