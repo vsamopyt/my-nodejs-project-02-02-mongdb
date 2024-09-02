53da418981eadefe745424a37f4194300ceafbfa
v.samopyt <v.samopyt14@gmail.com>
2024-09-02 21:19:14 +0300

    commit-04
    working with env

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import pino from "pino-http";
+import {env} from "./utils/env.js";
 
-const PORT =6000;
+// const PORT =6000;
+const PORT = Number(env('PORT', '3000'));
 
 export const setupServer =()=>{
     const app = express();
